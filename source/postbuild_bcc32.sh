4dfa1c4099cc2c6e30a3c29355b544fc08408079
Totte Karlsson <tottek@gmail.com>
2018-03-21 16:51:11 -0700

    Getting installer going

--- source/postbuild_bcc32.sh
+++ source/postbuild_bcc32.sh
@@ -18,37 +18,39 @@ echo "BDSCOMMONDIR: "$BDSCOMMONDIR
 
 #Extract version
 VERSION=`cat ../VERSION.txt | tr -d '\r'`
-APPNAME=atDB
+APPNAME=ArduinoController
 echo "Copy stuff"
 mkdir $APP_RELEASES/$APPNAME
 
 dest=$APP_RELEASES/$APPNAME
 
 cp ./../*.txt $dest
-cp $OUTPUTDIR/$OUTPUTFILENAME $dest
+cp $OUTPUTDIR/$OUTPUTFILENAME                   $dest
 
 #DS Binaries
- cp $OUTPUTDIR/mtkCommon.dll                    $dest
- cp $OUTPUTDIR/mtkMath.dll                      $dest
- cp $OUTPUTDIR/mtkIPC.dll                       $dest
- cp $OUTPUTDIR/atCore.dll                       $dest
- cp $OUTPUTDIR/atDatabase.dll                   $dest
- cp $OUTPUTDIR/atResources.dll                  $dest
- cp $OUTPUTDIR/atBarcodeReader.dll              $dest
- cp $OUTPUTDIR/atSSI.dll                        $dest
+cp $OUTPUTDIR/mtkCommon.dll                    $dest
+cp $OUTPUTDIR/mtkMath.dll                      $dest
+cp $OUTPUTDIR/mtkIPC.dll                       $dest
+cp $OUTPUTDIR/atCore.dll                       $dest
+cp $OUTPUTDIR/atDatabase.dll                   $dest
+cp $OUTPUTDIR/atSerialAPI.dll                   $dest
+cp $OUTPUTDIR/atArduino.dll                   $dest
+#cp $OUTPUTDIR/atResources.dll                  $dest
+cp $OUTPUTDIR/atBarcodeReader.dll              $dest
+cp $OUTPUTDIR/atSSI.dll                        $dest
 
- cp $BDSCOMMONDIR/BPL/atVCLCore.bpl             $dest    
- cp $BDSCOMMONDIR/BPL/DuneComponentsDD.bpl      $dest
- cp $BDSCOMMONDIR/BPL/DuneFormsDD.bpl           $dest
- cp $BDSCOMMONDIR/BPL/VCLCommonDD.bpl           $dest
+cp $BDSCOMMONDIR/BPL/atVCLCoreBCC32D.bpl             $dest    
+cp $BDSCOMMONDIR/BPL/DuneComponentsBCC32D.bpl      $dest
+cp $BDSCOMMONDIR/BPL/DuneFormsBCC32D.bpl           $dest
+cp $BDSCOMMONDIR/BPL/VCLCommonBCC32D.bpl           $dest
 
 #Borland Packages and dll's
 BC_REDIST="/cygdrive/c/CodeGear/Tokyo/bin"
 
-cp $BC_REDIST/borlndmm.dll                      $dest
+#cp $BC_REDIST/borlndmm.dll                      $dest
 cp $BC_REDIST/cc32250mt.dll                     $dest
-cp $BC_REDIST/midas.dll                         $dest
-
+#cp $BC_REDIST/midas.dll                         $dest
+#
 suffix="250.bpl"
 cp $BC_REDIST/bindcomp$suffix                   $dest
 cp $BC_REDIST/bindcompvcl$suffix                $dest
@@ -57,7 +59,6 @@ cp $BC_REDIST/dbexpress$suffix                  $dest
 cp $BC_REDIST/dbrtl$suffix                      $dest
 cp $BC_REDIST/dbxcds$suffix                     $dest
 cp $BC_REDIST/DbxCommonDriver$suffix		    $dest
-cp $BC_REDIST/DbxDevartPostgreSQLDriver$suffix	$dest
 cp $BC_REDIST/DBXMySQLDriver$suffix		        $dest
 cp $BC_REDIST/dsnap$suffix                      $dest
 cp $BC_REDIST/rtl$suffix                        $dest
@@ -66,12 +67,13 @@ cp $BC_REDIST/vclactnband$suffix                $dest
 cp $BC_REDIST/vcldb$suffix                      $dest
 cp $BC_REDIST/vclimg$suffix                     $dest
 cp $BC_REDIST/vclx$suffix                       $dest
-
-miscRedist="/cygdrive/p/redist/atdb"
-cp $miscRedist/dbexppgsql40.dll             $dest
-cp $miscRedist/tsclib.dll                   $dest
+cp $BC_REDIST/DbxDevartPostgreSQLDriver$suffix	$dest
+#
+miscRedist="/cygdrive/p/redist"
+#cp $miscRedist/dbexppgsql40.dll             $dest
+#cp $miscRedist/tsclib.dll                   $dest
 cp $miscRedist/navusbapi.dll                $dest
-
-#for local bin folder
-
-cp $miscRedist/navusbapi.dll                $OUTPUTDIR
+#
+##for local bin folder
+#
+#cp $miscRedist/navusbapi.dll                $OUTPUTDIR
